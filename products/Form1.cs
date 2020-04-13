a34b721b0d1b3d889b03f30a21b54071d19f5bb0
margaritovamariya <margaritovamariya@gmail.com>
2020-04-13 20:35:00 +0300

    Something great

--- products/Form1.cs
+++ products/Form1.cs
@@ -16,5 +16,10 @@ namespace products
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           //hELLO WORD
+        }
     }
 }
