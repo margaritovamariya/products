e4f5109cee2b8e38a5e6ed35c73dd0731e09ee2c
margaritovamariya <margaritovamariya@gmail.com>
2020-04-14 18:50:56 +0300

    FirstChange

--- products/Form1.Designer.cs
+++ products/Form1.Designer.cs
@@ -28,21 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(375, 58);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(586, 301);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(62, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 67);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Creams";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(62, 180);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(183, 63);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "MakeUp";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(62, 293);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(183, 68);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Parfumes";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(62, 400);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(183, 72);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Card";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(375, 400);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(185, 80);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Add";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(375, 395);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(185, 85);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "Remove";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(757, 405);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(204, 80);
+            this.button7.TabIndex = 17;
+            this.button7.Text = "Order";
+            this.button7.UseCompatibleTextRendering = true;
+            this.button7.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1014, 535);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- products/Form1.cs
+++ products/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,7 +20,39 @@ namespace products
 
         private void Form1_Load(object sender, EventArgs e)
         {
-           //hELLO WORD
+            //hELLO WORDobject sender, EventArgs eobject sender, EventArgs e
+        }
+
+        SqlConnection dbCon = new SqlConnection();
+
+        public void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           dbCon = new SqlConnection(
+     "Server=localhost\\SQLEXPRESS; " +
+     "Database=products; " +
+     "Integrated Security=true");
+            dbCon.Open();
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+       
+            using (dbCon)
+            {
+                SqlCommand command = new SqlCommand(
+                  "SELECT COUNT(*) FROM creams", dbCon);
+
+                
+                //123
+                //string statement = "INSERT INTO passport (passport_id, passport) Values (1, 'Maria');";
+                //using (SqlCommand command1 = new SqlCommand(statement, dbCon))
+                //{
+                //    command1.ExecuteNonQuery();
+                //}
+            }
         }
     }
 }
